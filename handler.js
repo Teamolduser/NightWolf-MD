7d1ad0e1668ab578d38280afc55d5f8d636e1cb4
NightWolf <88887015+NightWolf324@users.noreply.github.com>
2022-08-01 11:17:26 +0700

    Fix limit chat

--- handler.js
+++ handler.js
@@ -790,7 +790,7 @@ module.exports = {
                                 console.error(e)
                             }
                         }
-                       if (m.limit) m.reply(+ m.limit + ' Limit used')
+                       //if (m.limit) m.reply(+ m.limit + ' Limit used')
                     }
                     break
                 }
